a6901587290f2023f46fe5a2aa2ea35fb227aa18
Mac-ziis <macg286@gmail.com>
2023-07-27 18:03:18 -0700

    add to the UI. WIP

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-// import 'bootstrap';
-// import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import SolarAge from './js/solar-age.js';
 import FutureBirthday from './js/future-birthday.js';
